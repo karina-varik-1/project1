8150617c77279c01406ff723d48b7528d18051e9
Karina Varik <karina-varik-1@users.noreply.github.com>
2026-09-22 13:43:28 +0300

    Tunni lõppu commit.

--- www3.js
+++ www3.js
@@ -1,17 +1,70 @@
 const http = require('http');
+//moodul URL-i parsimiseks
+const url = require('url');
+//moodul failide haldamiseks
+const path = require('path');
+const fs = require('fs').promises;
 const dateTimeET = require('./src/dateTimeET');
 const pageHead = '<!DOCTYPE html>\n<html lang="et">\n<head>\n\t<meta charset="utf-8">\n\t<title>Karina Varik, veevbiprogrammeerimine</title>\n</head>\n<body>\n';
+const pageBanner = '<img src="veebiprogrammeerimine_2026_ID.png" alt="Banner">\n';
 const pageBody = '\t<meta charset="utf-8">\n\t<h1>Karina Varik, veebiprogrammeerimine</h1>\n\t <p>See leht on loodud veebiprogrammeerimise kursusel <a href="https://www.tlu.ee">Tallinna Ülikoolis</a> ning ei sislda tõsiseltvõetavat sisu!</p>\n\t<p>Esialgu tutvusime lihtsalt HTML keelega, peatselt programmeerime.</p>\n\t<hr>';
 const pageFoot = '\n</body>\n</html>';
 
 
-http.createServer(function(req, res){
-	res.writeHead(200, {"Content-type": "text/html"});
-	res.write(pageHead);
-	res.write(pageBody);
-	res.write('<p>Nädalapäev: ' + dateTimeET.weekday() + '</p>');
-	res.write('<p>Täna on ' + dateTimeET.date() + '.<p>');
-	res.write('<p>Kellaaeg: ' + dateTimeET.time() + '<p/>');
-	res.write(pageFoot);
-	return res.end();
+http.createServer(async function(req, res){
+	//vaatan URL-i
+	console.log('Päring: ' + req.url);
+	let currentURL = url.parse(req.url, true);
+	console.log('Pärsituna: ' + currentURL.pathname);
+	//console.log('Pärsituna: ' + currentURL.hostname);
+	if(currentURL.pathname === '/'){
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.write(pageHead);
+		res.write(pageBanner);
+		res.write(pageBody);
+		res.write('<p>Nädalapäev: ' + dateTimeET.weekday() + '</p>');
+		res.write('<p>Täna on ' + dateTimeET.date() + '.<p>');
+		res.write('<p>Kellaaeg: ' + dateTimeET.time() + '<p/>');
+		res.write(pageFoot);
+		return res.end();
+	}
+	
+	else if (currentURL.pathname === '/vanasona'){
+		res.writeHead(200, {"Content-type": "text/html"});
+		res.write(pageHead);
+		res.write('\t<h1>Tänane Eesti vanasõna</h1>\n\t<p>Siin näed tänaseks päevaks loositud vanasõna.</p>\n\t<hr>');
+		res.write(pageFoot);
+		return res.end();
+	}
+	
+	else if (currentURL.pathname === '/veebiprogrammeerimine_2026_ID.png'){
+		//liidame kättesaamatu päris kataliig jms virtuaalsesks failiteeks
+		let bannerPath = path.join(__dirname, 'pic', currentURL.pathname);
+		try {
+			const data = await fs.readFile(bannerPath);
+			res.writeHead(200, {"Content-type": "image/png"});
+			return res.end(data);
+		} catch (err) {
+			res.writeHead(404, {"Content-type": "text/plain; charset=utf8"});
+			return res.end('Pilti ei leitud!');
+		}
+	}
+	
+	
+//	else if (currentURL.pathname === '/veebiprogrammeerimine_2026_ID'){
+//		//liidame kättesaamatu päris kataliig jms virtuaalsesks failiteeks
+//		let bannerPath = path.join(__dirname, 'pic', currentURL.pathname);
+//		fs.readFile(bannerPath, (err, data)=>{
+//			if(err){
+//				throw(err);
+//			} else {
+//				res.writeHead(200, {"Content-type": "image/png"});
+//				return res.end(data);
+//			}
+//		});
+	//}
+	
+	else {
+		return res.end('Viga 404! Ei leia sellist lehte!');
+	}
 }).listen(5299);
